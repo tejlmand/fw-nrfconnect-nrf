3b9662234c7a5b7d1e062aa796678e5bb160a4de
Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>
2019-05-20 11:06:27 +0200

    Delaying entropy init until first call to ensure library can be initialized first Ensuring that entropy drivers don't collide with nRF5x (if enabled) Adding subdirectory if CoNFIG_ENTROPY_CC310_RNG is set This commit will be squashed
    Signed-off-by: Frank Audun Kvamtrø <frank.kvamtro@nordicsemi.no>

--- drivers/entropy_cc310/entropy_cc310.c
+++ drivers/entropy_cc310/entropy_cc310.c
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (c) 2019 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
@@ -10,10 +10,8 @@
 #include <string.h>
 
 #include <zephyr.h>
+#include <entropy.h>
 
-#if CONFIG_ENTROPY_CC310
-
-#if defined(CONFIG_MBEDTLS)
 #if !defined(CONFIG_MBEDTLS_CFG_FILE)
 #include "mbedtls/config.h"
 #else
@@ -21,12 +19,12 @@
 #endif /* CONFIG_MBEDTLS_CFG_FILE */
 
 #include "mbedtls/entropy.h"
-
-LOG_MODULE_REGISTER(entropy_cc310, ENTROPY_CC310_LOG_LEVEL);
+#include "mbedtls/platform.h"
 
 struct entropy_cc310_rng_dev_data {
 	mbedtls_entropy_context context;
-}
+	uint32_t		is_initialized;
+};
 
 #define DEV_DATA(dev) \
 	((struct entropy_cc310_rng_dev_data*)(dev)->driver_data)
@@ -44,6 +42,15 @@ static int entropy_cc310_rng_get_entropy(struct device *dev, u8_t *buffer,
 
 	__ASSERT_NO_MSG(dev_data != NULL);
 
+	/** Initialize if this is the first call.
+	 *  It is assumed that mbedtls_platform_setup
+	 *  is called prior to this
+	 */
+	if(dev_data->is_initialized == 0) {
+		mbedtls_entropy_init(&dev_data->context);
+		dev_data->is_initialized = true;
+	}
+
 	/* Get entropy data */
 	res = mbedtls_entropy_func(dev_data, buffer, length);
 	return res;
@@ -51,16 +58,17 @@ static int entropy_cc310_rng_get_entropy(struct device *dev, u8_t *buffer,
 
 static int entropy_cc310_rng_init(struct device *dev)
 {
+	/** Real initialization is postponed until the caller
+	 *  is able to initialize the CC310 hardware by calling
+	 *  mbedtls_platform_setup
+	 */
 	struct entropy_cc310_rng_dev_data *dev_data;
-	struct entropy_cc310_rng_dev_cfg *cfg_data;
 
 	__ASSERT_NO_MSG(dev != NULL);
 
 	dev_data = DEV_DATA(dev);
 
 	__ASSERT_NO_MSG(dev_data != NULL);
-
-	mbedtls_entropy_init(dev_data->context);
 
 	return 0;
 }
@@ -69,15 +77,12 @@ static const struct entropy_driver_api entropy_cc310_rng_api = {
 	.get_entropy = entropy_cc310_rng_get_entropy
 };
 
-static const struct entropy_cc310_rng_dev_cfg entropy_cc310_rng_config = {0,};
-
 static struct entropy_cc310_rng_dev_data entropy_cc310_rng_data = {0,};
 
 DEVICE_AND_API_INIT(entropy_cc310_rng, CONFIG_ENTROPY_NAME,
 		    entropy_cc310_rng_init,
 		    &entropy_cc310_rng_data,
-		    &entropy_cc310_rng_config,
+		    NULL,
 		    PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
 		    &entropy_cc310_rng_api);
 
-#endif /* CONFIG_ENTROPY_CC310 */
