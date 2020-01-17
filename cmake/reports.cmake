1070cfd61b3b2d8f5a3b1f2cf2ebdb6f9b08b5c6
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2020-01-17 12:48:24 +0100

    cmake: Add partition manager report as dependency to rom_report target.
    This commit uses the rom_report_DEPENDENCIES to add it self as
    dependency to the rom_report target in Zephyr.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/reports.cmake
+++ cmake/reports.cmake
@@ -4,12 +4,16 @@
 # SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
 #
 
-add_custom_command(
-  TARGET rom_report
-  POST_BUILD
+add_custom_target(
+  partition_manager_report
   COMMAND
   ${PYTHON_EXECUTABLE}
   ${ZEPHYR_BASE}/../nrf/scripts/partition_manager_report.py
   --input ${CMAKE_BINARY_DIR}/partitions.yml
   "$<$<NOT:$<TARGET_EXISTS:partition_manager>>:--quiet>"
   )
+
+set_property(TARGET zephyr_property_target
+             APPEND PROPERTY rom_report_DEPENDENCIES
+             partition_manager_report
+	     )
