a8d228184a534e073c682fd5ff08d5f080a8d455
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2021-07-13 15:44:34 +0200

    cmake: native NCS support for <sample>/configuration/<board> configs
    This commit introduces native support for configurations under:
    <sample>/configuration/<board>
    
    This scheme is used be several applications today, such as Pelion Client
    and nRF Desktop.
    
    Supporting this natively in NCS makes it possible to cleanup
    CMakeLists.txt files and also ensure a consistent structure among
    samples when this scheme is used.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/boilerplate.cmake
+++ cmake/boilerplate.cmake
@@ -28,3 +28,31 @@ if(DEFINED NRF_SUPPORTED_BUILD_TYPES)
                 message(FATAL_ERROR "${CMAKE_BUILD_TYPE} variant is not supported")
         endif()
 endif()
+
+string(FIND "${BOARD}" "@" REVISION_SEPARATOR_INDEX)
+if(NOT (REVISION_SEPARATOR_INDEX EQUAL -1))
+  math(EXPR BOARD_REVISION_INDEX "${REVISION_SEPARATOR_INDEX} + 1")
+  string(SUBSTRING ${BOARD} ${BOARD_REVISION_INDEX} -1 BOARD_REVISION)
+  string(SUBSTRING ${BOARD} 0 ${REVISION_SEPARATOR_INDEX} BOARD)
+endif()
+
+if(EXISTS ${CMAKE_SOURCE_DIR}/configuration)
+
+  if((DEFINED CONF_FILE) AND ("${CONF_FILE}" MATCHES "^configuration/${BOARD}/"))
+    # We have a relative conf file pointing inside configuration/<BOARD> folder
+    # hence old custom scheme is in use, thus do nothing until all samples have
+    # been updated to new scheme.
+  else()
+    set(APPLICATION_SOURCE_DIR ${CMAKE_SOURCE_DIR}/configuration/${BOARD}
+        CACHE PATH "Application Configuration Directory" FORCE
+    )
+    set(KCONFIG_ROOT ${CMAKE_SOURCE_DIR}/Kconfig)
+
+    if (NOT EXISTS "${APPLICATION_SOURCE_DIR}")
+      message(FATAL_ERROR
+              "Board ${BOARD} is not supported.\n"
+              "Please make sure board specific configuration files are added to "
+              "${CMAKE_CURRENT_SOURCE_DIR}/configuration/${BOARD}")
+    endif()
+  endif()
+endif()
